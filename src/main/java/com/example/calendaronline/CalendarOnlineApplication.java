03c53e8815880930354cfa5bdf0f24f8e2076178
katia_valentini <katia.valentini@adesso.it>
2026-06-04 10:41:15 +0200

    Initial commit

---
+++ src/main/java/com/example/calendaronline/CalendarOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.calendaronline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarOnlineApplication.class, args);
+    }
+
+}
